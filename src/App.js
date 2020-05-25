5b4ceead52beab6a0499a33c1f006a45e1ed30cc
David Trussler <mail@davidtrussler.net>
2020-05-25 09:17:09 +0100

    Demonstrates Higher Order Component - Adds ProFeature component to wrap SortedList component - Updates App component to conditionally display SortedList

--- src/App.js
+++ src/App.js
@@ -1,31 +1,70 @@
 import React, {Component} from 'react';
 import {GeneralList} from './GeneralList'; 
 import {SortedList} from './SortedList'; 
+import {ProFeature} from './ProFeature'; 
+
+const ProList = ProFeature(SortedList); 
 
 export default class App extends Component {
   constructor(props) {
     super(props); 
 
     this.state = {
-      // counter: 0
-      names: ['Zoe', 'Bob', 'Alice', 'Dora', 'Joe']
+      names: ['Zoe', 'Bob', 'Alice', 'Dora', 'Joe'], 
+      cities: ['London', 'Paris', 'New York', 'Munich'], 
+      proMode: false
     }
+  }
+
+  toggleProMode = () => {
+    this.setState({proMode: !this.state.proMode}); 
   }
 
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-          <div className="col-6">
+          <div className="col-12 text-center p-2">
+            <div className="form-check">
+              <label className="form-check-label">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  value={this.state.proMode}
+                  onChange={this.toggleProMode}
+                />
+                Pro Mode
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-3">
             <GeneralList
               list={this.state.names}
               theme="primary"
             />
           </div>
 
-          <div className="col-6">
-            <SortedList
+          <div className="col-3">
+            <ProList
               list={this.state.names}
+              pro={this.state.proMode}
+            />
+          </div>
+
+          <div className="col-3">
+            <GeneralList
+              list={this.state.cities}
+              theme="secondary"
+            />
+          </div>
+
+          <div className="col-3">
+            <ProList
+              list={this.state.cities}
+              pro={this.state.proMode}
             />
           </div>
         </div>
