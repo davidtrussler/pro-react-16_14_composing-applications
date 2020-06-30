7b683aa6ab96233315ab53d043b707937bcbc617
David Trussler <mail@davidtrussler.net>
2020-06-30 19:55:59 +0100

    Higher-order components - Demonstrates Higher Order Component - Demonstrates Stateful Higher Order Component - Demonstrates combining Higher Order Components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
 import React, {Component} from 'react';
 import {GeneralList} from './GeneralList'; 
 import {SortedList} from './SortedList'; 
+import {ProController} from './ProController'; 
+import {LogToConsole} from './LogToConsole'; 
+
+const ProList = ProController(LogToConsole(SortedList, 'Sorted', true, true, true)); 
 
 export default class App extends Component {
   constructor(props) {
     super(props); 
 
     this.state = {
-      // counter: 0
-      names: ['Zoe', 'Bob', 'Alice', 'Dora', 'Joe']
+      names: ['Zoe', 'Bob', 'Alice', 'Dora', 'Joe'], 
+      cities: ['London', 'Paris', 'New York', 'Munich']
     }
+  }
+
+  toggleProMode = () => {
+    this.setState({proMode: !this.state.proMode}); 
   }
 
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-          <div className="col-6">
+          <div className="col-3">
             <GeneralList
               list={this.state.names}
               theme="primary"
             />
           </div>
 
-          <div className="col-6">
-            <SortedList
+          <div className="col-3">
+            <ProList
               list={this.state.names}
+            />
+          </div>
+
+          <div className="col-3">
+            <GeneralList
+              list={this.state.cities}
+              theme="secondary"
+            />
+          </div>
+
+          <div className="col-3">
+            <ProList
+              list={this.state.cities}
             />
           </div>
         </div>
