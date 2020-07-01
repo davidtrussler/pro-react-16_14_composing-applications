797fa33d522d5806677278157a094238f418732c
David Trussler <mail@davidtrussler.net>
2020-07-01 06:26:44 +0100

    Using Hooks to consume context

--- src/ProModeToggle.js
+++ src/ProModeToggle.js
@@ -1,26 +1,24 @@
-import React, {Component} from 'react'; 
+import React, {useContext} from 'react'; 
 import {ProModeContext} from './ProModeContext'; 
 
-export class ProModeToggle extends Component {
-	static contextType = ProModeContext; 
+export function ProModeToggle(props) {
+	const context = useContext(ProModeContext); 
 
-	render() {
-		return (
-			<ProModeContext.Consumer>
-				{contextData => (
-					<div className="form-check">
-						<input
-							type="checkbox"
-							className="form-check-input"
-							value={this.context.proMode}
-							onChange={this.context.toggleProMode}
-						/>
-						<label className="form-check-label">
-							{this.props.label}
-						</label>
-					</div>
-				)}
-			</ProModeContext.Consumer>
-		)
-	}
+	return (
+		<ProModeContext.Consumer>
+			{contextData => (
+				<div className="form-check">
+					<input
+						type="checkbox"
+						className="form-check-input"
+						value={context.proMode}
+						onChange={context.toggleProMode}
+					/>
+					<label className="form-check-label">
+						{props.label}
+					</label>
+				</div>
+			)}
+		</ProModeContext.Consumer>
+	)
 }
