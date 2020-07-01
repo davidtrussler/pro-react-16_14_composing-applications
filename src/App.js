489632cf10203d26d64574c7eb09c8dbdf2700f1
David Trussler <mail@davidtrussler.net>
2020-07-01 05:21:44 +0100

    Using Render Props - Updates components to use render prop - Adds argument to render prop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import {GeneralList} from './GeneralList'; 
 import {SortedList} from './SortedList'; 
-import {ProController} from './ProController'; 
-import {LogToConsole} from './LogToConsole'; 
+import {ProFeature} from './ProFeature'; 
+// import {ProController} from './ProController'; 
+// import {LogToConsole} from './LogToConsole'; 
 
-const ProList = ProController(LogToConsole(SortedList, 'Sorted', true, true, true)); 
+// const ProList = ProController(LogToConsole(SortedList, 'Sorted', true, true, true)); 
 
 export default class App extends Component {
   constructor(props) {
@@ -12,7 +13,8 @@ export default class App extends Component {
 
     this.state = {
       names: ['Zoe', 'Bob', 'Alice', 'Dora', 'Joe'], 
-      cities: ['London', 'Paris', 'New York', 'Munich']
+      cities: ['London', 'Paris', 'New York', 'Munich'], 
+      proMode: false
     }
   }
 
@@ -24,29 +26,36 @@ export default class App extends Component {
     return (
       <div className="container-fluid">
         <div className="row">
-          <div className="col-3">
+          <div className="col-12 text-center p-2">
+            <div className="form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                value={this.state.proMode}
+                onChange={this.toggleProMode}
+              />
+              <label className="form-check-label">Pro Mode</label>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-6">
             <GeneralList
               list={this.state.names}
               theme="primary"
             />
           </div>
 
-          <div className="col-3">
-            <ProList
-              list={this.state.names}
-            />
-          </div>
-
-          <div className="col-3">
-            <GeneralList
-              list={this.state.cities}
-              theme="secondary"
-            />
-          </div>
-
-          <div className="col-3">
-            <ProList
-              list={this.state.cities}
+          <div className="col-6">
+            <ProFeature
+              pro = {this.state.proMode}
+              render = {text => 
+                <React.Fragment>
+                  <h4 className="text-center">{text}</h4>
+                  <SortedList list={this.state.names}/>
+                </React.Fragment>
+              }
             />
           </div>
         </div>
